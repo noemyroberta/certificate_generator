6fe29398f1b7a9da45c26e75f122a7b958e4e240
Noemy Roberta <noemy.oliveira2@outlook.com>
2024-05-13 12:43:43 -0300

    feat: add architecture first folder `ui`

---
+++ lib/src/ui/home/certificate_gen.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class CertificateGen extends StatelessWidget {
+  const CertificateGen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Scaffold();
+  }
+}
