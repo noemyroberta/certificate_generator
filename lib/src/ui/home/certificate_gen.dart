146a2bef697f43d7404bc15f450635cb96d3cdb3
Noemy Roberta <noemy.oliveira2@outlook.com>
2024-05-13 12:54:55 -0300

    feat: configure enviroment

--- lib/src/ui/home/certificate_gen.dart
+++ lib/src/ui/home/certificate_gen.dart
@@ -5,6 +5,11 @@ class CertificateGen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const Scaffold();
+    return MaterialApp(
+      title: 'WIT Certificate Gen',
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData.light(),
+      home: const Scaffold(),
+    );
   }
 }
