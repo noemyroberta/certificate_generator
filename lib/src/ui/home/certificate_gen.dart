d4f1edf319de9900c937f8f9c2c3542afbbe541a
Noemy Roberta <noemy.oliveira2@outlook.com>
2024-05-25 20:43:13 -0300

    feat: add header and first section of file sending

--- lib/src/ui/home/certificate_gen.dart
+++ lib/src/ui/home/certificate_gen.dart
@@ -1,15 +1,103 @@
 import 'package:flutter/material.dart';
+import 'package:wit_md_certificate_gen/src/ui/widgets/colors.dart';
+import 'package:wit_md_certificate_gen/src/ui/widgets/strings.dart';
 
 class CertificateGen extends StatelessWidget {
   const CertificateGen({super.key});
 
   @override
   Widget build(BuildContext context) {
+    final size = MediaQuery.sizeOf(context);
+
     return MaterialApp(
       title: 'WIT Certificate Gen',
       debugShowCheckedModeBanner: false,
       theme: ThemeData.light(),
-      home: const Scaffold(),
+      home: Scaffold(
+        backgroundColor: Colors.transparent,
+        body: Column(
+          children: [
+            SizedBox(
+              height: size.height * 0.1,
+              width: double.infinity,
+              child: ColoredBox(
+                color: primaryColor,
+                child: Padding(
+                  padding: const EdgeInsets.symmetric(horizontal: 20.0),
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                    children: [
+                      const Text(
+                        'Gerador de Certificado',
+                        style: TextStyle(
+                          color: Colors.white,
+                          fontFamily: 'RobotoSlab',
+                          fontWeight: FontWeight.normal,
+                          fontSize: 25,
+                        ),
+                      ),
+                      Image.asset(
+                        "assets/logo-laccan-completa-branca.png",
+                        width: 250,
+                      ),
+                    ],
+                  ),
+                ),
+              ),
+            ),
+            SizedBox(height: size.height * 0.2),
+            Container(
+              color: Colors.white70,
+              padding: const EdgeInsets.all(40),
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.start,
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  const Text(
+                    backgroundTitleText,
+                    style: TextStyle(
+                      color: Colors.black87,
+                      fontFamily: 'RobotoSlab',
+                      fontWeight: FontWeight.w500,
+                      fontSize: 25,
+                    ),
+                  ),
+                  const Text(
+                    backgroundSubtitleText,
+                    style: TextStyle(
+                      color: Colors.black54,
+                      fontFamily: 'RobotoSlab',
+                      fontStyle: FontStyle.italic,
+                      fontWeight: FontWeight.w400,
+                      fontSize: 14,
+                    ),
+                  ),
+                  const SizedBox(height: 10),
+                  ElevatedButton.icon(
+                    style: ButtonStyle(
+                      foregroundColor: MaterialStateColor.resolveWith(
+                          (states) => primaryColor),
+                      backgroundColor: MaterialStateColor.resolveWith(
+                          (states) => primaryColor),
+                    ),
+                    onPressed: () {},
+                    icon: const Icon(Icons.photo, color: Colors.white),
+                    label: const Text(
+                      'Inserir fundo',
+                      style: TextStyle(
+                        color: Colors.white,
+                        fontFamily: 'RobotoSlab',
+                        fontWeight: FontWeight.normal,
+                        fontSize: 14,
+                      ),
+                    ),
+                  ),
+                ],
+              ),
+            )
+          ],
+        ),
+      ),
     );
   }
 }
---
+++ lib/src/ui/widgets/strings.dart
@@ -0,0 +1,3 @@
+const backgroundTitleText = 'Insira o fundo do certificado';
+const backgroundSubtitleText =
+    'O fundo deve conter todas as imagens, assinaturas, cores\ne composições do fundo de todos os certificados.';
